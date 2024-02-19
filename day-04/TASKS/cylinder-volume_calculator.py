02d6e31303706d589a21b7d0710112fb23a0155e
Rhome Louie D. Saringayat <142566452+kurho@users.noreply.github.com>
2024-02-19 22:35:14 +0800

    Update cylinder-volume_calculator.py

--- day-04/TASKS/cylinder-volume_calculator.py
+++ day-04/TASKS/cylinder-volume_calculator.py
@@ -4,26 +4,22 @@ import math
 # Be cautious when reading input of various data types.
 
 
-import math
-
-print("HYPOTENUSE CALCULATOR FOR ALF")
-
-side1 = float(input("Please, enter the length of side A: "))
-while side1 < 0:
+print("CYLINDER VOLUME CALCULATOR FOR ALF")
+radius = float(input("Please, enter the radius of the cylinder: "))
+while radius < 0:
     print("Invalid input")
-    side1 = float(input("Please, enter the length of side A: "))
-
-side2 = float(input("Please, enter the length of side B: "))
-while side2 < 0:
+    radius = float(input("Please, enter the radius of the cylinder: "))
+height = float(input("Please, enter the radius of the cylinder: "))
+while height < 0:
     print("Invalid input")
-    side2 = float(input("Please, enter the length of side B: "))
-
+    height = float(input("Please, enter the radius of the cylinder: "))
 # Write the code ↓ to calculate the volume of the cylinder using the formula V = πr^2h.
 # Formula to calculate the volume (V) of a cylinder:
 # V = π * r^2 * h
 
 
-hypotenuse = math.sqrt((side1 * side1) + (side2 * side2))
+
+volume = math.pi * (radius * radius) * height
 
 
 
@@ -31,7 +27,7 @@ hypotenuse = math.sqrt((side1 * side1) + (side2 * side2))
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
-print("The hypotenuse of the right-angled triangle is: %.2f" %(hypotenuse))
+print("The volume of the cylinder is: %.2f" %(volume))
 
 
 
