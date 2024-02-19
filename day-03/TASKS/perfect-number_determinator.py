12778a0587cfa95723df01e1bd1b47e48ed8b2b5
Rhome Louie D. Saringayat <142566452+kurho@users.noreply.github.com>
2024-02-19 20:21:51 +0800

    Update perfect-number_determinator.py

--- day-03/TASKS/perfect-number_determinator.py
+++ day-03/TASKS/perfect-number_determinator.py
@@ -9,6 +9,12 @@
 
 
 
+print("PERFECT NUMBER DETERMINATOR FOR ALF")
+number = int(input("Please, enter a positive integer: "))
+while number <= 0:
+    print("Invalid input")
+    number = int(input("Please, enter a non-negative integer: ")) 
+result = number
 
 # Write the code ↓ to check if the entered integer is a Perfect Number using a loop.
 
@@ -17,11 +23,19 @@
 
 
 
+perfect = 0
+for i in range(1, number):
+    if number % i == 0:
+        perfect = perfect + i
 
 # Write the code ↓ to display the Perfect Number check result.
 # NOTE : You can use if-else statement or ternary operator to display the result.
 
 
+if perfect == result:
+    print("%d is a Perfect Number: "%(result))
+else:
+    print("%d is not a Perfect Number: " %(result))
 
 
 
